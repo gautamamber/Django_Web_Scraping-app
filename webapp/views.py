1a719e14eb9e1e76e16e28dd7778dc65879d62ed
gautamamber <ambergautam1@gmail.com>
2018-10-14 01:17:39 +0530

    Price api call

--- webapp/views.py
+++ webapp/views.py
@@ -6,7 +6,10 @@ import  requests
 # Create your views here.
 
 def home(request):
+	# crypto price data
+	price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD,EUR")
+	price = json.loads(price_request.content.decode('utf-8'))
 	# cryptocompare news api
 	api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
 	api = json.loads(api_request.content.decode('utf-8'))
-	return render(request, "webapp/home.html" , {'api' : api}) 
+	return render(request, "webapp/home.html" , {'api' : api, 'price' : price}) 
