efac6ab736c97e7f9fe83d0811165337282c0c9c
gautamamber <ambergautam1@gmail.com>
2018-10-14 01:00:23 +0530

    api calling and scrap the data

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+# converted into json
+import json
+# request the api
+import  requests
 # Create your views here.
 
 def home(request):
-	return render(request, "webapp/home.html")
+	# cryptocompare news api
+	api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+	api = json.loads(api_request.content.decode('utf-8'))
+	return render(request, "webapp/home.html" , {'api' : api}) 
