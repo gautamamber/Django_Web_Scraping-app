9405be2b7c28c91e88cc09d1c537c1a077dcce03
gautamamber <ambergautam1@gmail.com>
2018-10-14 01:39:34 +0530

    History api call

--- webapp/views.py
+++ webapp/views.py
@@ -13,3 +13,8 @@ def home(request):
 	api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
 	api = json.loads(api_request.content.decode('utf-8'))
 	return render(request, "webapp/home.html" , {'api' : api, 'price' : price}) 
+
+def history(request):
+	history_request = requests.get("https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10")
+	history = json.loads(history_request.content.decode('utf-8'))
+	return render(request, "webapp/history.html", {'history' : history})
